ad7e8646a9ecc38d89ae22c494f7f03d2112f882
kenta <bzmrchildren@icloud.com>
2023-05-30 20:58:15 +0900

    first-commit

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Author;
 use Illuminate\Http\Request;
+use App\Http\Requests\AuthorRequest;
 
 class AuthorController extends Controller
 {
@@ -12,4 +13,34 @@ class AuthorController extends Controller
     $authors = Author::all();
     return view('index', ['authors' => $authors]);
   }
+
+  public function find()
+  {
+    return view('find', ['input' => '']);
+  }
+
+  public function search(Request $request)
+  {
+    $author = Author::find($request->input);
+    $param = [
+      'author' => $author,
+      'input' => $request->input
+    ];
+    return view('find', $param);
+  }
+
+   // 追記：ここから
+  public function add()
+  {
+    return view('add');
+  }
+
+  public function create(AuthorRequest $request)
+  {
+    $form = $request->all();
+    Author::create($form);
+    return redirect('/');
+  }
+  // 追記：ここまで
 }
+
